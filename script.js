aa7700f3563b3e1a58354ac6e4cb4c42128b0fd0
cabritini <gustapacco@gmail.com>
2024-11-18 23:17:32 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const dataInicial = new Date("2024-09-21T18:48:00");
+
+function Contador() {
+    const dataAtual = new Date();
+    const diferenca = dataAtual - dataInicial;
+
+    const diasPassados = Math.floor(diferenca / (1000 * 60 * 60 * 24));
+    const horasPassadas = Math.floor((diferenca % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutosPassados = Math.floor((diferenca % (1000 * 60 * 60)) / (1000 * 60));
+    const segundosPassados = Math.floor((diferenca % (1000 * 60)) / 1000);
+
+    document.querySelector("#dias").innerHTML = String(diasPassados).padStart(2, '0');
+    document.querySelector("#horas").innerHTML = String(horasPassadas).padStart(2, '0');
+    document.querySelector("#minutos").innerHTML = String(minutosPassados).padStart(2, '0');
+    document.querySelector("#segundos").innerHTML = String(segundosPassados).padStart(2, '0');
+}
+
+setInterval(Contador, 1000);
